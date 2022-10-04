b83300e661af9aad555a9e085e5c0fda262d6d63
UddhavNavneeth <uddhav.navneeth2017@vitstudent.ac.in>
2022-10-04 01:59:19 -0700

    adding core logic of converting and redirecting

--- server.js
+++ server.js
@@ -10,8 +10,23 @@ app.set('view engine', hbs);
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.render('landingPage.hbs');
-})
+    let errorMessage = "";
+    res.render('landingPage.hbs', {errorMessage});
+});
+
+app.post('/convert', (req, res) => {
+    const input = req.body.input;
+    console.log(`input received for input: ${input}`);
+
+    txttosl.translate(input, 'BSL').then((url) => {
+        console.log(`url generated after conversion: ${url}`);
+        res.redirect(url);
+    }).catch((e) => {
+        const errorMessage = 'an error occured while converting';
+        res.render('landingPage.hbs', {errorMessage});
+        console.log(`conversion error: ${e}`);
+    })
+});
 
 app.listen(port, () => {
     console.log(`server is up on port ${port}`);
